8396aedd1a4a0a2db83467a5c8c522a7a4f0229f
TungLam-tech <tunglamweb@gmail.com>
2025-11-05 00:22:56 +0700

    supply sound and effect

--- main.js
+++ main.js
@@ -1,240 +1,114 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-const data = [
-  {
-    numberOrder: 1,
-    question: "",
-    urlImage: "./image/305.png",
-    answers: [
-      {
-        answer: "Biển 1",
-        correct: true,
-      },
-      {
-        answer: "Biển 2",
-        correct: false,
-      },
-      {
-        answer: "Biển 1 và 3",
-        correct: false,
-      },
-      {
-        answer: "Cả 3 biển",
-        correct: false,
-      },
-    ],
-  },
-  {
-    numberOrder: 2,
-    question: "",
-    urlImage: "./image/312.png",
-    answers: [
-      {
-        answer: "Biển 1 và 2",
-        correct: false,
-      },
-      {
-        answer: "Biển 2",
-        correct: false,
-      },
-      {
-        answer: "Biển 1 và 3",
-        correct: true,
-      },
-      {
-        answer: "Biển 2 và 3",
-        correct: false,
-      },
-    ],
-  },
-  {
-    numberOrder: 3,
-    question: "",
-    urlImage: "./image/326.png",
-    answers: [
-      {
-        answer: "Biển 1",
-        correct: false,
-      },
-      {
-        answer: "Biển 2",
-        correct: true,
-      },
-      {
-        answer: "Không biển nào",
-        correct: false,
-      },
-      {
-        answer: "Cả 2 biển",
-        correct: false,
-      },
-    ],
-  },
-  {
-    numberOrder: 4,
-    question: "",
-    urlImage: "./image/333.png",
-    answers: [
-      {
-        answer: "Biển 1",
-        correct: false,
-      },
-      {
-        answer: "Biển 2",
-        correct: false,
-      },
-      {
-        answer: "Cả ba Biển",
-        correct: true,
-      },
-      {
-        answer: "Biển 3",
-        correct: false,
-      },
-    ],
-  },
-  {
-    numberOrder: 5,
-    question: "",
-    urlImage: "./image/369.png",
-    answers: [
-      {
-        answer: "Biển 1",
-        correct: false,
-      },
-      {
-        answer: "Biển 2",
-        correct: true,
-      },
-      {
-        answer: "Cả biển 1 và biển 2",
-        correct: false,
-      },
-      {
-        answer: "Không biển nào",
-        correct: false,
-      },
-    ],
-  },
-  {
-    numberOrder: 6,
-    question: "",
-    urlImage: "./image/372.png",
-    answers: [
-      {
-        answer: "Biển 1",
-        correct: false,
-      },
-      {
-        answer: "Biển 1 và 3",
-        correct: false,
-      },
-      {
-        answer: "Biển 3",
-        correct: true,
-      },
-      {
-        answer: "Cả ba biển",
-        correct: false,
-      },
-    ],
-  },
-  {
-    numberOrder: 7,
-    question: "",
-    urlImage: "./image/387.png",
-    answers: [
-      {
-        answer: "Biển 1 và 3",
-        correct: false,
-      },
-      {
-        answer: "Biển 2",
-        correct: true,
-      },
-      {
-        answer: "Biển 3",
-        correct: false,
-      },
-      {
-        answer: "Không biển nào",
-        correct: false,
-      },
-    ],
-  },
-  {
-    numberOrder: 8,
-    question: "",
-    urlImage: "./image/396.png",
-    answers: [
-      {
-        answer: "Biển 1",
-        correct: false,
-      },
-      {
-        answer: "Biển 2",
-        correct: true,
-      },
-      {
-        answer: "Biển 3",
-        correct: false,
-      },
-      {
-        answer: "Cả 3 biển",
-        correct: false,
-      },
-    ],
-  },
-  {
-    numberOrder: 9,
-    question: "",
-    urlImage: "./image/432.png",
-    answers: [
-      {
-        answer: "Biển 1",
-        correct: true,
-      },
-      {
-        answer: "Biển 2",
-        correct: false,
-      },
-      {
-        answer: ".",
-        correct: false,
-      },
-      {
-        answer: ".",
-        correct: false,
-      },
-    ],
-  },
-  {
-    numberOrder: 10,
-    question: "",
-    urlImage: "./image/435.png",
-    answers: [
-      {
-        answer: "Chỉ hướng đi phải theo",
-        correct: false,
-      },
-      {
-        answer:
-          "Biển báo hiệu cho người lái xe biết số lượng làn đường trên mặt đường và hướng đi trên mỗi làn đường phải theo",
-        correct: true,
-      },
-      {
-        answer: "Chỉ hướng đường phải theo",
-        correct: false,
-      },
-      {
-        answer: ".",
-        correct: false,
-      },
-    ],
-  },
-];
+const soundCorrect = new Audio("./sound/correct.mp3");
+const soundWrong = new Audio("./sound/wrong.mp3");
+const soundClick = new Audio("./sound/click.mp3");
+const soundCheer = new Audio("./sound/cheer.mp3");
+const soundSad = new Audio("./sound/sad.mp3");
+// fetch data from quiz json
+let startIndex = 0; //không được xóa vì liên quan đến fetch
+let endIndex = 10; //không được xóa vì liên quan đến fetch
+let data = [];
+let dataOfQuestionSet = [];
 
+fetch("./quizData.json")
+  .then((res) => res.json())
+  .then((result) => {
+    data = result;
+    dataOfQuestionSet = data.slice(startIndex, endIndex);
+    start();
+  })
+  .catch((err) => {
+    console.error("Lỗi khi load quizData.json:", err);
+  });
+//----------------------------------------------------------------------------------
+const listQuestionBlock = $(".list-question-block");
+// function hiển thị số lượng bộ đề
+function createNumberOderQuestionSet() {
+  let num = Math.ceil(data.length / 10); // luôn chuẩn rồi. ko được chỉnh, cứ thêm 10 câu hỏi thì tạo 1 bộ đề
+  for (let i = 0; i < num; i++) {
+    listQuestionBlock.innerHTML += `<span class="list-question" data-number="${i}">Đề số ${
+      i + 1
+    }</span>`;
+  }
+}
+
+// --------------------------------------------------------------------
+// function tăng startIndex và endIndex lên mỗi lần 10
+function raiseStartEndIndex(num) {
+  startIndex = num * 10;
+  endIndex = startIndex + 10;
+  dataOfQuestionSet = data.slice(startIndex, endIndex);
+}
+
+//------------------------------------------------------------------
+//  tra loi xong quay lai man hinh chinh
+const chooseListQuestionBtn = $(".btn-chooseListQuestion"); // nút chọn đề
+
+chooseListQuestionBtn.addEventListener("click", () => {
+  soundClick.play();
+  start();
+});
+// -------------------------------------------------------------------------
+
+// -----------------------------------------------------------------------------------
+// function man hinh chinh (start())
+const blockAnswer = $(".block-answer");
+
+function start() {
+  listQuestionBlock.innerHTML = ""; // reset giao diện
+  createNumberOderQuestionSet(); // tạo lại các bộ đề
+
+  const listQuestion = $$(".list-question"); // lấy danh sách sau khi đã tạo
+
+  const allResults = JSON.parse(localStorage.getItem("pass")) || {};
+
+  listQuestion.forEach((question, index) => {
+    if (allResults.hasOwnProperty(index)) {
+      if (allResults[index] === 100) {
+        question.style.color = "aqua";
+        question.innerHTML = `✓ Đề số ${index + 1}`;
+      } else {
+        question.style.color = "#ff6b3eff";
+        question.innerHTML = `✗ Đề số ${index + 1}`;
+      }
+    } else {
+      question.style.color = "";
+      question.innerHTML = `Đề số ${index + 1}`;
+    }
+
+    question.addEventListener("click", () => {
+      soundClick.play();
+      raiseStartEndIndex(index);
+      init(begin);
+      localStorage.setItem("selected index", JSON.stringify(index));
+      blockAnswer.classList.remove("hide");
+      blockQuestion.classList.remove("hide");
+      nextBtn.classList.add("hide");
+      alertCorrectOrIncorrect.classList.add("hide");
+      blockChoice.forEach((choice) => {
+        choice.classList.remove("hide");
+      });
+    });
+  });
+
+  begin = 0;
+  score = 0;
+  isClick = false;
+  reset();
+
+  blockAnswer.classList.add("hide");
+  blockQuestion.classList.add("hide");
+  nextBtn.classList.add("hide");
+  alertCorrectOrIncorrect.classList.add("hide");
+  blockResult.classList.add("hide");
+  showResultBtn.classList.add("hide");
+}
+
+// ------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------
 const blockQuestion = $(".block-question");
 const blockChoice = $$(".choice");
 const showResultBtn = $("#show-result");
@@ -249,18 +123,19 @@ const imgElement = $(".image");
 // -------------------------------------------------------------------
 // function init
 function init(begin) {
-  blockQuestion.innerText = `Câu ${data[begin].numberOrder}/${data.length}: ${data[begin].question}
+  listQuestionBlock.innerHTML = ""; // để bộ câu hỏi ẩn đi
+  blockQuestion.innerText = `Câu số ${dataOfQuestionSet[begin].numberOrder}: ${dataOfQuestionSet[begin].question}
 `;
-  imgElement.src = data[begin].urlImage;
+  imgElement.src = dataOfQuestionSet[begin].urlImage;
   blockQuestion.appendChild(imgElement);
   blockChoice.forEach((choice, index) => {
     choice.innerHTML = `<div class="letter">${letters[index]}</div>
-                    <div class="text-choice">${data[begin].answers[index].answer}</div>`;
+                    <div class="text-choice">${dataOfQuestionSet[begin].answers[index].answer}</div>`;
   });
-  showResultBtn.setAttribute("hidden", true);
-  blockResult.setAttribute("hidden", true);
-  nextBtn.setAttribute("hidden", true);
-  alertCorrectOrIncorrect.setAttribute("hidden", true);
+  showResultBtn.classList.add("hide");
+  blockResult.classList.add("hide");
+  nextBtn.classList.add("hide");
+  alertCorrectOrIncorrect.classList.add("hide");
 }
 // ----------------------------------------------------------------------------
 // reset
@@ -273,14 +148,30 @@ function reset() {
 
 // function next question
 const stt = $(".stt");
-function next() {
-  begin++;
-  init(begin);
-  reset();
-  stt.scrollIntoView(true);
-  alertYouChoose.classList.add("hide");
-}
 
+function next() {
+  soundClick.play();
+  // Ẩn bằng fade
+  blockQuestion.classList.remove("visible");
+  blockAnswer.classList.remove("visible");
+  blockQuestion.classList.add("fade");
+  blockAnswer.classList.add("fade");
+
+  setTimeout(() => {
+    begin++;
+    init(begin);
+    reset();
+
+    // Hiện lại bằng fade-in
+    blockQuestion.classList.remove("fade");
+    blockAnswer.classList.remove("fade");
+    blockQuestion.classList.add("visible");
+    blockAnswer.classList.add("visible");
+
+    stt.scrollIntoView(true);
+    alertYouChoose.classList.add("hide");
+  }, 400);
+}
 nextBtn.addEventListener("click", next);
 //----------------------------------------------------------------------------------
 // function check answer correct
@@ -296,36 +187,46 @@ blockChoice.forEach((choice, index) => {
     alertYouChoose.innerText = `Bạn chọn ${letters[index]}`;
     alertYouChoose.classList.remove("hide");
     // correct set green
-    if (data[begin].answers[index].correct === true) {
+    if (dataOfQuestionSet[begin].answers[index].correct === true) {
+      soundCorrect.play();
+
       choice.classList.add("green-correct");
       alertCorrectOrIncorrect.innerText = " Đúng!";
       alertCorrectOrIncorrect.style.color = "#2fff00ff";
-      alertCorrectOrIncorrect.removeAttribute("hidden");
-      nextBtn.removeAttribute("hidden");
+      alertCorrectOrIncorrect.classList.remove("hide");
+      nextBtn.classList.remove("hide");
       score++; // correct raise score by 1
       alertCorrectOrIncorrect.scrollIntoView(true); // view focus in true or false
 
       //  other incorrect set red
     } else {
       // add red incorrect
+
       choice.classList.add("red-incorrect");
+
       alertCorrectOrIncorrect.innerText = `Sai!`;
-      alertCorrectOrIncorrect.style.color = "#ff8800ff";
-      alertCorrectOrIncorrect.removeAttribute("hidden");
-      nextBtn.removeAttribute("hidden");
+      alertCorrectOrIncorrect.style.color = "#ff9100ff";
+      alertCorrectOrIncorrect.classList.remove("hide");
+      nextBtn.classList.remove("hide");
+      soundWrong.play();
+
+      // đặt ngay sau âm thanh để đồng bộ âm thanh và rung
+      if ("vibrate" in navigator) {
+        navigator.vibrate(200);
+      } // rung nhẹ 200ms
 
       // add green correct
       blockChoice.forEach((otherChoice, otherIndex) => {
-        if (data[begin].answers[otherIndex].correct === true) {
+        if (dataOfQuestionSet[begin].answers[otherIndex].correct === true) {
           otherChoice.classList.add("green-correct");
         }
       });
       alertCorrectOrIncorrect.scrollIntoView(true); //view focus in true or false
     }
     // Nếu chưa phải câu cuối
-    if (begin == data.length - 1) {
-      showResultBtn.removeAttribute("hidden");
-      nextBtn.setAttribute("hidden", true);
+    if (begin == dataOfQuestionSet.length - 1) {
+      showResultBtn.classList.remove("hide");
+      nextBtn.classList.add("hide");
     }
   });
 });
@@ -339,7 +240,7 @@ function showResult() {
   });
   alertCorrectOrIncorrect.classList.add("hide");
   showResultBtn.classList.add("hide");
-  blockResult.removeAttribute("hidden");
+  blockResult.classList.remove("hide");
   showScore();
   alertYouChoose.classList.add("hide");
 }
@@ -347,16 +248,17 @@ function showResult() {
 showResultBtn.addEventListener("click", showResult);
 // -----------------------------------------------------------------
 
-// function close-and-again
+// function nút làm lại
 
-const closeBtn = $(".btn-close");
+const closeAgainBtn = $(".btn-close");
 
-closeBtn.addEventListener("click", () => {
+closeAgainBtn.addEventListener("click", () => {
+  soundClick.play();
   begin = 0; // quay về câu đầu
   isClick = false; // cho phép click lại
   score = 0;
   // ẩn phần kết quả
-  blockResult.setAttribute("hidden", true);
+  blockResult.classList.add("hide");
 
   // hiển thị lại các phần đã bị .hide
   blockQuestion.classList.remove("hide");
@@ -376,14 +278,22 @@ const scoreBlock = $("#score");
 const paraScore = $(".para-score");
 const conclusionBlock = $(".conclusion");
 function showScore() {
-  percentScore = Math.round((score / data.length) * 100);
+  percentScore = Math.round((score / dataOfQuestionSet.length) * 100);
   scoreBlock.innerText = `${percentScore}%`;
   scoreBlock.style.color = percentScore >= 80 ? "#3967e6ff" : "red";
-  paraScore.innerText = `Bạn đã trả lời đúng ${score} trên ${data.length} câu hỏi`;
-  // conclusionBlock.innerText = percentScore >= 50 ? `Bạn đã đỗ` : `Bạn đã trượt`;
+  paraScore.innerText = `Bạn đã trả lời đúng ${score} trên ${dataOfQuestionSet.length} câu hỏi`;
+
+  const selectedIndex = JSON.parse(localStorage.getItem("selected index"));
+  let allResults = JSON.parse(localStorage.getItem("pass")) || {};
+  allResults[selectedIndex] = percentScore;
+  localStorage.setItem("pass", JSON.stringify(allResults));
+  if (percentScore === 100) {
+    soundCheer.play();
+  } else {
+    soundSad.play();
+  }
 }
 // --------------------------------------------------------------------------
 //  pháo hoa
-
-// khoi tao
-init(begin);
+//------------------------------------------------------------------------------------
+//
